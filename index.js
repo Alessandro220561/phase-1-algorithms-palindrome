6f9b1f35a514ae1589f734ddf09dd86afec9fda9
Alessandro Calvera <alessandro.calvera@gmail.com>
2023-04-19 13:46:01 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const string = word;
+  const reverseString = word.split('').reverse('').join('')
+  if(reverseString === string) {
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
-  Add your pseudocode here
+  save original string into a variable.
+  use split, reverse,  and join to make that string be read backwards,
+  check value of backwards string with regular string and return boolean.
 */
 
 /*
-  Add written explanation of your solution here
+  I saved the parameter into a variable,
+  then into another variable I first used .split() to split the string into an array of individual characters,
+  then I used .reverse() to reverse the order in which that string is naturally read,
+  then I used .join() to join together that backwards array into a string which i checked if it is equal 
+  to the original string.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  // I expect that running racecar will return true
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
-
+  // I expect that running robot will return false  
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
